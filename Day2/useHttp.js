46b817b9332f20a8eece1efe386b5b53edb89e11
ptomer40 <ptomer40@yahoo.com>
2025-01-30 09:26:53 +0530

    fetchAPINode

--- Day2/useHttp.js
+++ Day2/useHttp.js
@@ -11,13 +11,15 @@ const server=http.createServer((req,res)=>{
     res.setHeader('Content-Type','text/html')
   
     
-    const response=fetch("https://jsonplaceholder.typicode.com/comments?postId=1");
+    //const response=fetch("https://jsonplaceholder.typicode.com/comments?postId=1");
+    const response=fetch("https://dummyjson.com/products");
     console.log(response);
     response.then((data)=>{
         console.log(data);
         data.json().then((res1)=>{
-            console.log(res1);
+            console.log(res1.products);
             //res.end(JSON.stringify(res1));
+            /*
             res.end(`
                 <html>
                 <head>JSON DATA
@@ -35,6 +37,39 @@ const server=http.createServer((req,res)=>{
                 </body>
                 </html>
                 `);
+                */
+                res.write(`<html>
+                    <style>
+                        body{
+                            padding: 2rem;
+                            background-color: yellow;
+                            display: flex;
+                            flex-direction: column;
+                            gap: 2rem;
+                        }
+
+                        div{
+                            width: 400px;
+                            background: lime;
+                            padding: 2rem;
+                        }
+                    </style>
+            <body>`);
+
+    res1.products.forEach((elem) => {
+        res.write(`
+            <div>
+                <h1>${elem.title}</h1>
+                <p>${elem.description}</p>
+                <img src="${elem.thumbnail}" height='200'>
+            </div>
+        `);
+    });
+
+    res.end(`</body>
+        </html>`);
+
+
         })
     })
    
